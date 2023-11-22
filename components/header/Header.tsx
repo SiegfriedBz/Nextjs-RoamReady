0e1ce7437eb3360ea6c4568ac10d8e7fcbb085d3
siegfriedbz <siegfried.bozza@yahoo.com>
2023-11-22 08:28:05 +0100

    add Header Footer + css

---
+++ components/header/Header.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import Logo from './Logo'
+import DesktopNav from './DesktopNav'
+import MobileNav from './MobileNav'
+
+const Header = () => {
+  return (
+    <div id='header'>
+      <div className='flex gap-x-4'>
+        <Logo />
+        BrandName
+      </div>
+
+      {/* desktop nav */}
+      <div>
+        <DesktopNav />
+      </div>
+
+      {/* mobile nav */}
+      <div>
+        <MobileNav />
+      </div>
+    </div>
+  )
+}
+
+export default Header
