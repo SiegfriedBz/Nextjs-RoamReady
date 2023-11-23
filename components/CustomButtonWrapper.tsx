1c789ce413df25cbee6cef1f3cdee89ea3ba56ed
siegfriedbz <siegfried.bozza@yahoo.com>
2023-11-23 04:22:39 +0100

    add CustomButtonWrapper

---
+++ components/CustomButtonWrapper.tsx
@@ -0,0 +1,31 @@
+'use client'
+
+import { useRouter } from 'next/navigation'
+import CustomButton from './buttons/CustomButton'
+
+type Props = {
+  variant?: string
+  className?: string
+  route: string
+  children: React.ReactNode
+}
+const CustomButtonWrapper = ({
+  variant,
+  className,
+  route,
+  children,
+}: Props) => {
+  const router = useRouter()
+
+  return (
+    <CustomButton
+      variant={variant}
+      className={className}
+      handleClick={() => router.push(route)}
+    >
+      {children}
+    </CustomButton>
+  )
+}
+
+export default CustomButtonWrapper
